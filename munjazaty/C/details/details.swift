dda888b39cd5d69bb7d0200e680178d371a74508
obada <obada@10.0.2.15>
2018-12-28 21:33:37 -0800

    last28-finally-withoutviedo

---
+++ munjazaty/C/details/details.swift
@@ -0,0 +1,30 @@
+//
+//  details.swift
+//  munjazaty
+//
+//  Created by obada on 12/26/18.
+//  Copyright © 2018 obada. All rights reserved.
+//
+
+import UIKit
+
+class details: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ munjazaty/m/resultofdetails.swift
@@ -0,0 +1,13 @@
+//
+//  resultofdetails.swift
+//  munjazaty
+//
+//  Created by obada on 12/28/18.
+//  Copyright © 2018 obada. All rights reserved.
+//
+
+import UIKit
+
+class resultofdetails: UICollectionViewCell {
+    
+}
