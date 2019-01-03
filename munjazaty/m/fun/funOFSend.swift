c747f11003891eaf2269727c358f7d85896bbf8d
obada <obada@10.0.2.15>
2019-01-02 21:08:50 -0800

    last2/1-all

---
+++ munjazaty/m/fun/funOFSend.swift
@@ -0,0 +1,21 @@
+//
+//  funOFSend.swift
+//  munjazaty
+//
+//  Created by obada on 1/2/19.
+//  Copyright © 2019 obada. All rights reserved.
+//
+
+import UIKit
+
+class funOFSend: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
